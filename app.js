82cbac4ae0f55f4d8df1b9d23229b1baf431b95c
EDLyonhart <EDLyonhart@gmail.com>
2015-03-19 09:33:03 -0700

    First bonus complete

--- app.js
+++ app.js
@@ -12,21 +12,36 @@ app.use(methodOverride("_method"));
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
-  client.SMEMBERS("students", function(err, pupils) {
-    res.render("index", {students: pupils});
+  client.SMEMBERS("students", function(err, students) {
+    res.render("index", {students: students});
   });
 });
 
+//create a student
 app.post("/create", function(req, res){
   client.SADD("students", req.body.name);
   res.redirect("/");
 });
 
+//remove all students
 app.delete("/remove", function(req, res){
   client.del("students");
   res.redirect("/");
 });
 
+//remove specific student
+app.delete("/remove/:student", function(req, res){
+  client.SMEMBERS("students", function(err, students){
+    students.forEach(function(student){
+      if(req.params.student === student){
+        client.SREM("students", student);
+        res.redirect("/");
+      }
+    });
+  });
+});
+
+//start server
 app.listen(3000, function(){
   console.log("Server startin on port 3000");
 });
